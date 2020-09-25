f11619e1a3a8f796cda45924f496a905ad01b35c
Mohammad Danyal <danyal.io@outlook.com>
2020-09-25 09:10:20 +0100

    Initial commit

---
+++ Anagram/Solver/Main.cs
@@ -0,0 +1,21 @@
+﻿using Anagram.Solver;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace Anagram
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Anagram Solver!");
+            Console.WriteLine("Enter a word: ");
+            string word = Console.ReadLine();
+            WordList wordList = new WordList(word);
+        }
+    }
+}
---
+++ Anagram/Solver/PairCalculator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anagram.Solver
+{
+
+    /**
+    * Class which works out the appropriate pairs of words.
+    *
+    * @author Mohammad Danyal
+    * @version September 2020
+    */
+
+    class PairCalculator
+    {
+        private List<string> possibleWords;
+
+        /**
+* 
+* @param possibleWords holds a list of all the words that are of the correct length and alphabet.
+* @return the pairs of words which are of the correct length.
+*/
+        public PairCalculator(List<string> possibleWords, string mainWord)
+        {
+            this.possibleWords = possibleWords;
+            GetPairs(mainWord);
+        }
+
+        public List<string> GetPairs(string mainWord)
+        {
+
+            List<string> possiblePairs = new List<string>();
+            List<string> Pairs = new List<string>();
+
+            foreach (String firstPossibleWord in possibleWords)
+            {
+                foreach (String secondPossibleWord in possibleWords)
+                {
+                    if ((firstPossibleWord.Length + secondPossibleWord.Length) == mainWord.Length)
+                    {
+                        possiblePairs.Add(firstPossibleWord + ' ' + secondPossibleWord);
+                    }
+                }
+            }
+
+
+            foreach (String possiblePair in possiblePairs)
+            {
+
+                string currentPair = possiblePair;
+                string possiblePairNoSpaces = possiblePair.Replace(" ", "");
+
+                char[] ch1 = mainWord.ToCharArray();
+                char[] ch2 = possiblePairNoSpaces.ToCharArray();
+
+                Array.Sort(ch1);
+                Array.Sort(ch2);
+
+                String val1 = new string(ch1);
+                String val2 = new string(ch2);
+
+
+
+                if (val1 == val2)
+                {
+                    Pairs.Add(currentPair);
+                }
+
+            }
+
+            return Pairs;
+        }
+
+    }
+}
+
