1066ff1524a1b0b4243965fa19b95099b980adf3
Mohammad Danyal <danyal.io@outlook.com>
2020-10-06 22:33:47 +0100

    Create extension method for pairing sort

---
+++ Anagram/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anagram.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string SortAlphabetically(this string str)
+        {
+            char[] ch2 = str.ToCharArray();
+
+            Array.Sort(ch2);
+
+            var val2 = new string(ch2);
+
+            return val2;
+        }
+    }
+}
