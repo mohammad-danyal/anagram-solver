376fa4346b459a8f63219fbda273f3c3043d0e7e
Mohammad Danyal <mohammad.danyal@zenith.co.uk>
2020-11-12 15:29:48 +0000

    Add and configure NSwag middleware

--- AnagramSolverAPI/Controllers/AnagramSolverController.cs
+++ AnagramSolverAPI/Controllers/AnagramSolverController.cs
@@ -1,5 +1,8 @@
 ﻿using AnagramSolverAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+using System;
 using System.Collections.Generic;
 
 namespace AnagramSolverAPI.Controllers
@@ -12,13 +15,27 @@ namespace AnagramSolverAPI.Controllers
 
         public AnagramSolverController(IAnagramSolver solver)
         {
-            _solver = solver;
+            _solver = solver ?? throw new System.ArgumentNullException(nameof(solver));
         }
 
-        [HttpGet("GetAnagrams/{word}")]
-        public List<Pair> GetAnagrams(string word)
+        [HttpGet("GetAnagrams/{word}", Name = "GetAnagrams")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [OpenApiOperation("GetAnagrams")]
+        public IActionResult GetAnagrams(string word)
         {
-            return _solver.FindAnagrams(word);
+            if (String.IsNullOrWhiteSpace(word))
+            {
+                BadRequest("Bad Request: No word has been specified");
+            }
+            var anagrams = _solver.FindAnagrams(word);
+
+            if (anagrams is null) 
+            {
+                return NotFound();
+            } else { 
+                return Ok(anagrams);
+            }            
         }
     }
 }
